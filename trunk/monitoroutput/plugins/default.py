1732231877d634e4a296c48572e2754a096b9f4f
scottakirkwood <scottakirkwood@c18886aa-2b2d-0410-8335-df332501ff58>
2007-04-24 13:23:22 +0000

    Fixed up the --mop argument passing

--- trunk/monitoroutput/plugins/default.py
+++ trunk/monitoroutput/plugins/default.py
@@ -88,26 +88,20 @@ class TestPlugin(_plugin.TestCommand):
             out = self.mo.handle_line(line)
             self.assertEquals(name, out)
 
-        def testUnless(self):
-            lines = [
-                ('Error', "It's not an error if it's an ErrorClass"),
-                ('Error', "Or if it's ErrorException"),
-            ]
-            for name, line in lines:
-                out = self.mo.handle_line(line)
-                self.assertEquals(line, out)
+    def testUnless(self):
+        lines = [
+            ('Error', "It's not an error if it's an ErrorClass"),
+            ('Error', "Or if it's ErrorException"),
+        ]
+        for name, line in lines:
+            out = self.mo.handle_line(line)
+            self.assertEquals(line, out)
 
     def testWarning(self):
         line = "] WARNING: blabla"
         out = self.mo.handle_line(line)
         self.assertEquals('\033[0;34m] WARNING: blabla\033[0m', out)
 
-    def testThreadWarning(self):
-        line = 'Exception in thread "main" com.google.common.base.Flags$FlagUpdateError: '
-        out = self.mo.handle_line(line)
-
-    def testThreadWarning(self):
-        out = self.mo.handle_exit()
 
 if __name__ == "__main__":
     import unittest
